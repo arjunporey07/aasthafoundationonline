a95893f76f929579be3efbe6a69883b66a52abd0
arjunporey07 <arjunporey07@gmail.com>
2021-08-08 13:19:12 +0530

    set url as localhost

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://github.com/arjunporey07/aasthafoundationonline.git';
+$config['base_url'] = 'http://localhost/aasthafoundationonline/';
 
 /*
 |--------------------------------------------------------------------------
